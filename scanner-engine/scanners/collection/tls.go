a627f322a01b57070b676c51c7f7434bbaf807f4
Tirth9000 <sharmatirth09012@gmail.com>
2026-03-21 12:32:13 +0530

    Refactor scanner registration and enhance data collection for mail security and service detection

--- scanner-engine/scanners/collection/tls.go
+++ scanner-engine/scanners/collection/tls.go
@@ -152,6 +152,7 @@ func (f *TLSDataCollection) RunCollectionScanner(
 
 			for i := range ports {
 				if ports[i].Port == port {
+					fmt.Println(ports[i].Product, ports[i].Version)
 					ports[i].TLS = tlsData
 				}
 			}
